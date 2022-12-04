acc0439d431b766881d76b81f2e81a543d754552
bunkergames <bunkergames@protonmail.com>
2022-12-04 17:39:16 +0100

    Add more nonsense stuff

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # install
+
+```bash
+$ curl -s https://raw.githubusercontent.com/bunkergames/install/main/deploy.sh | bash
+```
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo "Hello World!"
+version="v0.1.0"
+rootdir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+selfname=$(basename "$(readlink -f "${BASH_SOURCE[0]}")")
+sessionname=$(echo "${selfname}" | cut -f1 -d".")
+gamesdir="${rootdir}/gameservers"
+scriptsdir="${rootdir}/scripts"
+helpersdir="${rootdir}/helpers"
+dbdir="${rootdir}/db"
+logdir="${rootdir}/log"
+localdir="${rootdir}/.local"
+tmpdir="${rootdir}/tmp"
+
+# Check that curl is installed before doing anything
+if [ ! "$(command -v curl 2>/dev/null)" ]; then
+	echo -e "[ FAIL ] Curl is not installed"
+	exit 1
+fi
+
+echo "rootdir: "$rootdir
+echo "selfname: "$selfname
+echo "sessionname: "$sessionname
